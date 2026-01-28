b7d090ff6dcf7c8d85fa4f5c0d720e68cd86e447
workramsah <workramsah@gmail.com>
2026-01-28 20:39:11 +0545

    fix logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,12 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
-      <Image
-        className="cursor-pointer w-28 md:w-32"
-        onClick={() => router.push('/')}
-        src={assets.logo}
-        alt="logo"
-      />
+      <img src="/imga.png" className="cursor-pointer w-28 md:w-32"></img>
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
         <Link href="/" className="hover:text-gray-900 transition">
           Home
